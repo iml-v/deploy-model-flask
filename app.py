b0116ecc30046a99dba986c9bc49364cb32ed021
iml-v <bishnoimlv@gmail.com>
2020-02-21 15:05:14 +0530

    updated request form

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from flask import Flask, jsonify, request
+import flask
+from flask import Flask, render_template,jsonify, request
 import pickle
+import numpy as np
 
 # load model
 model = pickle.load(open('model.pkl','rb'))
@@ -9,24 +11,50 @@ model = pickle.load(open('model.pkl','rb'))
 app = Flask(__name__)
 
 # routes
-@app.route('/', methods=['POST'])
 
-def predict():
+@app.route('/')
+
+@app.route('/index')
+
+def index():
+    return flask.render_template('index.html')
+
+def predict(to_predict_list):
     # get data
-    data = request.get_json(force=True)
+    # data = request.get_json(force=True)
+    #
+    # # convert data into dataframe
+    # data.update((x, [y]) for x, y in data.items())
+    # data_df = pd.DataFrame.from_dict(data)
+    #
+    # # predictions
+    # result = model.predict(data_df)
+    #
+    # # send back to browser
+    # output = {'results': int(result[0])}
+    #
+    # # return data
+    # return jsonify(results=output)
+    to_predict = np.array(to_predict_list).reshape(1, 4)
+    loaded_model = pickle.load(open("model.pkl", "rb"))
+    result = model.predict(to_predict)
+    return result[0]
 
-    # convert data into dataframe
-    data.update((x, [y]) for x, y in data.items())
-    data_df = pd.DataFrame.from_dict(data)
+@app.route('/result',methods=['POST'])
+def results():
+    if request.method =='POST':
+        to_predict_list= request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int,to_predict_list))
+        results = predict(to_predict_list)
 
-    # predictions
-    result = model.predict(data_df)
+        if int(results) == 1:
+            prediction = 'Income more than 50K'
+        else:
+            prediction = 'Income less that 50K'
 
-    # send back to browser
-    output = {'results': int(result[0])}
+        return render_template("results.html", prediction=prediction)
 
-    # return data
-    return jsonify(results=output)
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
